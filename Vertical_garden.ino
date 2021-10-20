ca357c58fba91beece774ccaf23a18a096bf36ad
peter <peter9167@naver.com>
2021-10-20 23:30:24 +0900

    three commit

--- Vertical_garden.ino
+++ Vertical_garden.ino
@@ -1,8 +1,8 @@
 #include <TimerOne.h>
 
 #include <SoftwareSerial.h>        // 블루투스 시리얼 통신 라이브러리 추가
-#define BT_RXD 4
-#define BT_TXD 5
+#define BT_RXD 7
+#define BT_TXD 8
 SoftwareSerial BTserial(BT_RXD, BT_TXD);        // 블루투스 설정 BTSerial(Tx, Rx)
 
 #include <DHT.h>
@@ -13,9 +13,11 @@ DHT dht(DHTPIN, DHTTYPE);
 int pir = 3; //인체 감지 센서
 int fire = 4; //불꽃 감지 센서
 int sound = 5; //소리 감지 센서
- 
+
 int light = A0; //조도 센서
-int soil = A1; //토양 수분 센서
+
+int led = 13;
+int pump = 12;
 
 //-------타이머 인터럽트를 위한 타이머 카운트------------
 int time_count = 0;
@@ -23,6 +25,7 @@ int hum_count = 0;
 int temp_count = 0;
 int light_count = 0;
 int soil_count = 0;
+int bt_count = 0;
 
 //------모듈 사용---------
 int 
@@ -36,6 +39,8 @@ void setup(){
   pinMode(sound, INPUT);
   pinMode(light, INPUT);
   pinMode(soil, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(pump, OUTPUT);
   Timer1.initialize(100000); //1000000μs = 1s, 1000us = 0.001s, 1000us = 1ms
   Timer1.attachInterrupt(timerIsr); // run every 0.1 seconds
 }
@@ -44,6 +49,7 @@ void loop(){
   int h = dht.readHumidity(); //습도 값 저장 변수 선언
   int t = dht.readTemperature(); //온도 값 저장 변수 선언
   int l = analogRead(A0);
+  int bt = (bluetooth.read());
 
   //------습도 값 읽기 카운터-------
   if(hum_count == 4){
@@ -113,5 +119,12 @@ void PUMP(){
   else 
   {
     digitalWrite(13,LOW);
+  }
+}
+
+void BT(int bt){
+  if(bluetooth.available()){
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
   }
 }
