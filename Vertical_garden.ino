dad12f7e3622e9ec64fb0e20b7b423b3f803972d
peter <peter9167@naver.com>
2021-10-22 23:38:57 +0900

    five commit

--- Vertical_garden.ino
+++ Vertical_garden.ino
@@ -1,8 +1,8 @@
 #include <TimerOne.h>
 ///////////////////////////////////////////////////////////////////////////
 #include <SoftwareSerial.h>        // 블루투스 시리얼 통신 라이브러리 추가
-#define BT_RXD 7
-#define BT_TXD 8
+#define BT_RXD 5
+#define BT_TXD 6
 SoftwareSerial bluetooth(BT_RXD, BT_TXD); // 블루투스 설정 BTSerial(Tx, Rx)
 ///////////////////////////////////////////////////////////////////////////
 #include <DHT.h>
@@ -22,11 +22,13 @@ Servo servo;    // 서보모터 사용을 위한 객체 생성
 #define DELAYVAL 1
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 ///////////////////////////////////////////////////////////////////////////
+#include <Stepper.h>
+const int stepsPerRevolution = 64; // 모터별 스탭 수 설정 (28YBJ-48의 경우 64)
+Stepper stepper(stepsPerRevolution, 8, 10, 9, 11);
+///////////////////////////////////////////////////////////////////////////
 int light = A0; //조도 센서
-int pir = A1; //인체 감지 센서
 int fire = A2; //불꽃 감지 센서
-int sound = A3; //소리 감지 센서
-int motor = 6;
+int pir = 4; //인체 감지 센서
 
 int pump = 11;
 int fan = 12;
@@ -37,9 +39,7 @@ int hum_count = 0;
 int temp_count = 0;
 int pir_count = 0;
 int fire_count = 0;
-int sound_count = 0;
 int light_count = 0;
-int soil_count = 0;
 int bt_count = 0;
 
 //------모듈 사용---------
@@ -53,13 +53,10 @@ void setup(){
   bluetooth.begin(9600);
   dht.begin();
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-
-  servo.attach(motor);  // 서보모터 연결
-  servo.write(0);
+  stepper.setSpeed(500); // 속도 설정
   
   pinMode(pir, INPUT); //인체 감지 센서
   pinMode(fire, INPUT); //불꽃 감지 센서
-  pinMode(sound, INPUT); //소리 감지 센서
   pinMode(light, INPUT); //조도 센서
   pinMode(pump, OUTPUT); //pump
   pinMode(fan, OUTPUT); //쿨링팬
@@ -79,11 +76,10 @@ void setup(){
 void loop(){
   int h = dht.readHumidity();
   int t = dht.readTemperature();
-  int p = analogRead(pir);
+  int p = digitalRead(pir);
   int f = analogRead(fire);
-  int s = analogRead(sound);
   int l = analogRead(A0);
-  int bt = (bluetooth.read());
+  char bt = (bluetooth.read());
 
   //------습도 값 읽기 카운터-------
   if(hum_count == 4){
@@ -109,16 +105,15 @@ void loop(){
     fire_count = 0;
   }
 
-  //------소리(사운드) 감지 센서 값 읽기 카운터-------
-  if(sound_count == 8){
-    SOUND(s);
-    sound_count = 0;
-  }
-
   //------조도센서 값 읽기 카운터-------
   if(light_count == 9){
     LIGHT(l);
     light_count = 0;
+  }
+
+  //------블루투스 모듈 사용 카운터-------
+  if(light_count == 10){
+    BT(bt, h, t, p, f, l);
   }
 }
 
@@ -128,8 +123,8 @@ void timerIsr(){
   temp_count += 1;
   pir_count += 1;
   fire_count += 1;
-  sound_count += 1;
   light_count += 1;
+  bt_count += 1;
 }
 
 void hum(int h){
@@ -156,11 +151,9 @@ void PIR(int p){
   Serial.print("인체 감지:");
   Serial.println(p); // 인체 감지 값 출력
   if(p == 1){
-    for(int i = 20; i <= 120; i++){
-      servo.write(i);
-    }
+    stepper.step(5000);
   } else{
-    servo.write(20);
+    stepper.step(-5000);
   }
 }
 
@@ -171,14 +164,6 @@ void FIRE(int f){
     digitalWrite(pump, HIGH);
   } else{
     digitalWrite(pump, LOW);
-  }
-}
-
-void SOUND(int s){
-  Serial.print("소리:");
-  Serial.println(s); // 소리 감지 값 출력
-  if(s < 1200){
-    servo.write(100);
   }
 }
 
@@ -205,17 +190,50 @@ void PUMP(){
   if(++Pcount >= 10 )
   {
     Pcount = 0;
-    digitalWrite(13,HIGH);
+    digitalWrite(pump,HIGH);
   }
   else 
   {
-    digitalWrite(13,LOW);
+    digitalWrite(pump,LOW);
   }
 }
 
-void BT(int bt){
+void BT(char bt, int h, int t, int p, int f, int l){
+  bluetooth.write("온도 : ");
+  bluetooth.write(t);
+  bluetooth.write("'C");
+  bluetooth.write("습도 : ");
+  bluetooth.write(h);
+  bluetooth.write("%");
+  bluetooth.write("빛 밝기 : ");
+  bluetooth.write(l);
   if(bluetooth.available()){
-    digitalWrite(12,HIGH);
-    digitalWrite(13,HIGH);
+    switch(bt){
+      case '1':
+        for(int i=0; i<NUMPIXELS; i++) {
+          pixels.setPixelColor(i, pixels.Color(255, 150, 0));
+          pixels.show();
+          delay(DELAYVAL); // Pause before next pass through loop
+        }
+        break;
+      case '2':
+        for(int i=0; i<NUMPIXELS; i++) {
+          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+          pixels.show();
+        }
+        break;
+      case '3':
+        digitalWrite(pump, HIGH);
+        break;
+      case '4':
+        digitalWrite(pump, LOW);
+        break;
+      case '5':
+        stepper.step(5000);
+        break;
+      case '6':
+        stepper.step(-5000);
+        break;
+    }
   }
 }
